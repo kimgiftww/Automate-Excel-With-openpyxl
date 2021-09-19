498c6c9b2602dc4540915df9682629f337228966
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2021-09-19 10:36:42 +0800

    Add files via upload

--- lesson3_write_read_data.py
+++ lesson3_write_read_data.py
@@ -5,7 +5,6 @@ wb = xl.Workbook()
 ws = wb.active
 
 
-
 #########################
 #       Write Data       #
 #########################
@@ -42,13 +41,31 @@ row_insertion = ws.max_row
 
 for indx_row, record in enumerate(records):
     for indx_col, value in enumerate(record):
-        print(row_insertion + 1 + indx_row, column_insertion + indx_col)
+        # print(row_insertion + 1 + indx_row, column_insertion + indx_col)
         ws.cell(row_insertion + 1 + indx_row, column_insertion + indx_col, value=value)
 
 ##########################
 #       Read Data        #
 ##########################
 last_row = ws.max_row
+rng = ws['A1':'D4']
+rng2 = ws['A1:D4']
+cols = ws['B:C']
+# not recommended as it will iterate every single cell
+rows = ws['2:4']
+
+# to print the cell values
+for row in rng:
+    for cell in row:
+        print(cell.value)
+
+# we can also use the Worksheet.iter_rows() or Worksheet.iter_cols method
+# this method
+for row in ws.iter_rows(min_row=1, max_row=3, min_col=1, max_col=3):
+    for cell in row:
+        print('Cell Row: {0}, Col: {1}, Value: {2}'.format(cell.row, cell.column, cell.value))
+        print('Cell {0}{1} has value: {2}'.format(cell.column_letter, cell.row, cell.value))
+
 
 
 wb.save('lesson3.xlsx')
